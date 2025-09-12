5dc37d3b729531c8c21631b7f95101674012331a
Michael <mdumont567@gmail.com>
2025-09-12 11:36:55 -0300

    feat: Update API routes for user and transaction handling

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,20 @@ use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['comment' => 'Access allowed without authorization'], function () {
+Route::group(['comment' => 'APIs without authorization'], function () {
     Route::post('login', [UserController::class, "login"]);
     Route::post('deposit', [TransactionController::class, "createDeposit"]);
+    Route::post('user', [UserController::class, "create"]);
 });
 
 Route::middleware('auth:api')->group(function () {
-    Route::group(['prefix' => 'user'], function () {
-        Route::post('', [UserController::class, "create"]);
-        Route::get('', [UserController::class, "all"]);
-    });
-
     Route::group(['prefix' => 'transaction'], function () {
         Route::post('transfer', [TransactionController::class, "createTransfer"]);
-        Route::get('extract/{userId}', [TransactionController::class, "getExtractByUser"]);
+        Route::get('extract', [TransactionController::class, "getExtract"]);
     });
+});
+
+Route::group(['comment' => 'APIs for admin users'], function () {
+    Route::get('extracts', [TransactionController::class, "getExtracts"]);
+    Route::get('user', [UserController::class, "all"]);
 });
