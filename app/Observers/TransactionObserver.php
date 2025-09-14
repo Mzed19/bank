66c4199cfac6899857f2da2f9cb03c0887822efe
Michael <mdumont567@gmail.com>
2025-09-14 03:03:12 -0300

    Fix

--- app/Observers/TransactionObserver.php
+++ app/Observers/TransactionObserver.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 class TransactionObserver
 {
@@ -25,26 +26,29 @@ class TransactionObserver
             Log::critical("Não foi possível creditar um valor de $record->amount para o usuário $record->receiver_account_id");
             Log::critical($exception->getMessage());
             $this->revertTransaction(
-                tableName: $record->getTable,
+                tableName: $record->getTable(),
                 recordId: $record->id
             );
+            throw new UnprocessableEntityHttpException('Não foi possível concluir a transação.');
         }
 
-        if (!is_null($record->sender_id)) {
+        if (!is_null($record->sender_account_id)) {
             try {
                 Transaction::create([
-                    'account_id' => $record->sender_id,
+                    'account_id' => $record->sender_account_id,
                     'amount' => -$record->amount,
                     'type' => Str::of($record->getTable())->singular()->value(),
                     'imported_id' => $record->id
                 ]);
             } catch (Exception $exception) {
                 $this->revertTransaction(
-                    tableName: $record->getTable,
+                    tableName: $record->getTable(),
                     recordId: $record->id
                 );
-                Log::critical("Não foi possível debitar um valor de $record->amount para o usuário $record->sender_id");
+                Log::critical("Não foi possível debitar um valor de $record->amount para o usuário $record->receiver_account_id");
                 Log::critical($exception->getMessage());
+                throw new UnprocessableEntityHttpException('Não foi possível concluir a transação.');
+
             }
         }
     }
